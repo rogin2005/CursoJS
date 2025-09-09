3b6716814c04d5687d7cdbc5c3f7659d36198428
Roger Ferreira Saraiva <rogerferreirasaraiva@gmail.com>
2025-09-08 22:28:19 -0300

    update

--- CursoJS/aula11/ex008.js
+++ CursoJS/aula11/ex008.js
@@ -1,2 +1,6 @@
-var vel = 60.5
+var vel = 59
 console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) { // condição simples!
+    console.log('Você ultrapassou a velocidade máxima!')
+}
+console.log('Dirija com cuidado!')
---
+++ CursoJS/aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'neymar 2050'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil') { // se usarmos o "!=" dá para fazer a condição contrária que é igual a diferença
+    console.log('Vc é Brasileiro!')
+} else {
+    console.log('Vc é Estrangeiro!')
+}
