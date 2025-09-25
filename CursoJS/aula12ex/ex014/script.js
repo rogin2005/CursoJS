f045b61f6e41b7bb32b1a46db9506430bb6c8154
Roger Ferreira Saraiva <rogerferreirasaraiva@gmail.com>
2025-09-24 22:38:39 -0300

    concluindo ex014

--- CursoJS/aula12ex/ex014/script.js
+++ CursoJS/aula12ex/ex014/script.js
@@ -1,1 +1,21 @@
-alert('Olá')
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    hora = 19
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        // BOM DIA
+        img.src = 'fotomanha.png'
+        document.body.style.background = '#69baf5'
+    } else if (hora >= 12 && hora < 18) {
+        // BOA TARDE
+        img.src = 'fototarde.png'
+        document.body.style.background = '#f6ae7c'
+    } else {
+        // BOA NOITE!
+        img.src = 'fotonoite.png'
+        document.body.style.background = '#302a48'
+    }
+}
