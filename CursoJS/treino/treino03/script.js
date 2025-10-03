4a46c60b1758cc145242203707460d7367863f30
Roger Ferreira Saraiva <rogerferreirasaraiva@gmail.com>
2025-10-03 20:41:01 -0300

    treino 03 completo
    exceto a parte de gerar a imagem via js

--- CursoJS/treino/treino03/script.js
+++ CursoJS/treino/treino03/script.js
@@ -1,4 +1,25 @@
 function verMoto() {
     var opcoes = document.getElementsByName('radm')
-    var foto = document.createElement('img')
+    var foto = document.querySelector('img#foto')
+    var velocidade = document.querySelector('h3#velm')
+    var marcha = document.querySelector('h3#marcha')
+
+    foto.setAttribute('id', 'foto')
+    if (opcoes[0].checked) {
+        foto.setAttribute('src', 'bros.png')
+        velocidade.innerHTML = "Velocidade máxima: 130Km/h"
+        marcha.innerHTML = "Marchas: 5"
+    } else if (opcoes[1].checked) {
+        foto.setAttribute('src', 'factor.png')
+        velocidade.innerHTML = "Velocidade máxima: 140Km/h"
+        marcha.innerHTML = "Marchas: 5"
+    } else if (opcoes[2].checked) {
+        foto.setAttribute('src', 'bmw.png')
+        velocidade.innerHTML = "Velocidade máxima: 240Km/h"
+        marcha.innerHTML = "Marchas: 6"
+    } else if (opcoes[3].checked) {
+        foto.setAttribute('src', 'titan.png')
+        velocidade.innerHTML = "Velocidade máxima: 150Km/h"
+        marcha.innerHTML = "Marchas: 5"
+    }
 }
