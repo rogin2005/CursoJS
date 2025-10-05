a374815c2fb0664d235bbeb18fa70bfa2a2bb9fa
Roger Ferreira Saraiva <rogerferreirasaraiva@gmail.com>
2025-10-05 07:40:41 -0300

    while e do while

---
+++ CursoJS/aula13/ambiente.js
@@ -0,0 +1,34 @@
+var c = 1
+
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 1000)
+
+
+
+
+
+/*
+var c = 1
+
+while (c <= 1000) {
+    console.log(`Passo ${c}`)
+    c++
+} */
+
+
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
