f80e95897d81248b639834197062f538fe7441ee
Roger Ferreira Saraiva <rogerferreirasaraiva@gmail.com>
2025-10-30 22:35:16 -0300

    preparando ex016

--- CursoJS/aula14ex/ex016/script.js
+++ CursoJS/aula14ex/ex016/script.js
@@ -1,1 +1,14 @@
-alert('Olá')
+function Contar() {
+    var inicio = document.querySelector('input#ini').value
+    var fim = document.querySelector('input#fim').value
+    var passo = document.querySelector('input#pas').value
+    var resultado = document.querySelector('div#res')
+
+    if (condicao) {
+
+    }
+
+    for (inicio;fim;passo) {
+
+    }
+}
