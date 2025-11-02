7a6152305977334f30f5f505308077c58de3044a
Roger Ferreira Saraiva <rogerferreirasaraiva@gmail.com>
2025-11-01 22:23:55 -0300

    logica

--- CursoJS/aula14ex/ex016/script.js
+++ CursoJS/aula14ex/ex016/script.js
@@ -4,11 +4,16 @@ function Contar() {
     var passo = document.querySelector('input#pas').value
     var resultado = document.querySelector('div#res')
 
-    if (condicao) {
+    console.log(inicio)
+    console.log(fim)
+    console.log(passo)
 
+    if (!inicio || fim <= inicio || !passo || !fim) {
+        resultado.innerHTML = "Impossível contar!"
+    } else if (passo == 0) {
+        passo = 1
+        alert("Não dá para contar com passo 0, consdiderando passo 1 para continuar contagem!")
     }
-
-    for (inicio;fim;passo) {
-
-    }
+console.log(passo)
+    
 }
