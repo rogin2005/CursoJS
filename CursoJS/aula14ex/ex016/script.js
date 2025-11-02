df5b78f2dccc01723da26c49981d81c33cb3de18
Roger Ferreira Saraiva <rogerferreirasaraiva@gmail.com>
2025-11-02 15:22:18 -0300

    ex016 contagem completo

--- CursoJS/aula14ex/ex016/script.js
+++ CursoJS/aula14ex/ex016/script.js
@@ -1,9 +1,14 @@
 function Contar() {
-    var inicio = document.querySelector('input#ini').value
-    var fim = document.querySelector('input#fim').value
-    var passo = document.querySelector('input#pas').value
+    var ini = document.querySelector('input#ini').value
+    var fi = document.querySelector('input#fim').value
+    var pas = document.querySelector('input#pas').value
     var resultado = document.querySelector('div#res')
 
+    var inicio = Number(ini)
+    var fim = Number(fi)
+    var passo = Number(pas)
+
+    resultado.innerHTML = ""
 
     console.log(inicio)
     console.log(fim)
@@ -14,11 +19,10 @@ function Contar() {
         alert("Não dá para contar com passo 0, consdiderando passo 1 para continuar contagem!")
     }
 
-    if (!inicio || fim <= inicio || !passo || !fim) {
+    if ( inicio < 0 || fim <= inicio || !passo || !fim) {
         resultado.innerHTML = "Impossível contar!"
     } else {
-        resultado.innerHTML = ""
-        for (inicio; inicio <= fim; inicio++) {
+        for (inicio ; inicio <= fim; inicio += passo) {
             resultado.innerHTML += `${inicio} 👉`
         }
         resultado.innerHTML += "🏁"
