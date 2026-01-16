5b900c7453456ad3adb8ff24f6b7d677f5c3a7f4
Roger Ferreira Saraiva <rogerferreirasaraiva@gmail.com>
2026-01-16 08:14:34 -0300

    projeto alcool-gasolina finalizado utilizando boas práticas e principios do js

---
+++ CursoJS/Fs_udemy/alcool-gasolina/js/script.js
@@ -0,0 +1,32 @@
+function calcularMelhorPreco() {
+
+    //validar campos
+    let precoAlcool = document.getElementById('alcool').value
+    let precoGasolina = document.getElementById('gasolina').value
+
+    if (precoAlcool != '') {
+        if (precoGasolina != '') {
+
+        /*Calcular se é melhor usar álcool ou gasolina
+            * se valorAlcool / valorGasolina >= 0.7 é melhor utilizar Gasolina
+            * senão é melhor utilizar álcool
+        */
+
+        let resultado = precoAlcool / precoGasolina
+
+        if ( resultado >= 0.7 ) {
+            //alert("Melhor utilizar gasolina")
+            document.getElementById('resultado').innerHTML = "Melhor utilizar Gasolina"
+        } else {
+            //alert("Melhor utilizar álcool")
+            document.getElementById('resultado').innerHTML = "Melhor utilizar Álcool"
+        }
+
+        } else {
+            alert("Preencha o preço da gasolina!")
+        }
+    } else {
+        alert("Preencha o preço do alcool!")
+    }
+
+}
