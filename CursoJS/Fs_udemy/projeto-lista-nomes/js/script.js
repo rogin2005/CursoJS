f75a19fe4fac8c0222b2638ab94212c6ca9242f5
Roger Ferreira Saraiva <rogerferreirasaraiva@gmail.com>
2026-01-17 22:40:44 -0300

    projeto finalizado com pesquisa simples

--- CursoJS/Fs_udemy/projeto-lista-nomes/js/script.js
+++ CursoJS/Fs_udemy/projeto-lista-nomes/js/script.js
@@ -11,7 +11,27 @@ let nomes = [
     "carlos"
 ]
 
+function pesquisarNome() {
+
+    let nomePesquisa = document.getElementById('campoNome').value
+    let itensLista = ''
+
+
+    for( indice in nomes ){
+        
+        let nome = nomes[indice]
+        if( nomePesquisa == nome ) {
+            itensLista += `<li class="list-group-item">${nome}</li>`
+        }
+
+    } 
+    document.getElementById('lista').innerHTML = itensLista
+
+}
+
+
 function carregarNomes() {
+
     let itensLista = ''
     for( indice in nomes ){
         
