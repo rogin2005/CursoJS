f212f66bc416dc977d923279ba960f5451f94832
Roger Ferreira Saraiva <rogerferreirasaraiva@gmail.com>
2026-03-11 17:50:14 -0300

    let vs const ts

---
+++ CursoJS/Fs_udemy/typescript/src/modules/basic-types/let-vs-const.ts
@@ -0,0 +1,8 @@
+let phrase1 = 'Que a força esteja com você';
+
+//const phrase2: 'Como deve ser' = 'Como deve ser'
+const phrase2 = 'Como deve ser';
+
+let phrase3 = 'Como tenho dito' as const; //let convertido pra const
+
+export default () => {};
