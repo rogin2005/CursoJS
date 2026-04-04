cbfe3cc38413c76e2d8f34067de24040380d5e8d
Roger Ferreira Saraiva <rogerferreirasaraiva@gmail.com>
2026-04-04 16:01:55 -0300

    App vs Pages

---
+++ CursoJS/Fs_udemy/react-next-aula/react-fundamentos/src/pages/compras.tsx
@@ -0,0 +1,10 @@
+import { Cabecalho } from "@/components/interface";
+
+export default function Page() {
+    return (
+        <div>
+            { Cabecalho() }
+            <h1>Compras</h1>
+        </div>
+    )
+}
