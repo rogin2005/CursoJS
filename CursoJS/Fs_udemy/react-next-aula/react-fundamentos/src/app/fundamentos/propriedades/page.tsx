a952862c553bc616b0bc82dd5308e958f44fad87
Roger Ferreira Saraiva <rogerferreirasaraiva@gmail.com>
2026-04-04 16:51:20 -0300

    Componente como tag

---
+++ CursoJS/Fs_udemy/react-next-aula/react-fundamentos/src/app/fundamentos/propriedades/page.tsx
@@ -0,0 +1,11 @@
+import { Acomodacao, Cabecalho } from "@/components/interface";
+
+export default function Page() {
+    return(
+        <div>
+            <Cabecalho />
+            <hr />
+            <Acomodacao />
+        </div>
+    )
+}
