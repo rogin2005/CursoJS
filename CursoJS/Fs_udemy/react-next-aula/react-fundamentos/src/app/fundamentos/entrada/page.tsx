fe88abf3725e8e2453db7ce9832034f8c5fdca67
Roger Ferreira Saraiva <rogerferreirasaraiva@gmail.com>
2026-04-18 12:33:19 -0300

    Criando caixa de texto

---
+++ CursoJS/Fs_udemy/react-next-aula/react-fundamentos/src/app/fundamentos/entrada/page.tsx
@@ -0,0 +1,11 @@
+export default function Entrada() {
+    return (
+        <div className="p-2">
+            <h1>Entrada de dados</h1>
+            <input 
+            value={"Roger"}
+            className="campo-texto"
+            type="text" />
+        </div>
+    )
+}
