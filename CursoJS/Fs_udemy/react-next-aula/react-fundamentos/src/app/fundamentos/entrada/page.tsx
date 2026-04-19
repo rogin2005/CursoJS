6e55b23dff2c21c025b5ae8178e44caaa4b85189
Roger Ferreira Saraiva <rogerferreirasaraiva@gmail.com>
2026-04-19 16:36:53 -0300

    Estado de caixa de texto

--- CursoJS/Fs_udemy/react-next-aula/react-fundamentos/src/app/fundamentos/entrada/page.tsx
+++ CursoJS/Fs_udemy/react-next-aula/react-fundamentos/src/app/fundamentos/entrada/page.tsx
@@ -1,11 +1,28 @@
+'use client'
+
+import { useState } from "react"
+
 export default function Entrada() {
+
+    const [valor, setValor] = useState('')
+
+    function alterarValor( e: any ) {
+        console.log(e)
+        setValor( e.target.value )
+    }
+
     return (
         <div className="p-2">
             <h1>Entrada de dados</h1>
             <input 
-            value={"Roger"}
+            value={valor}
             className="campo-texto"
+            // onChange={ e => setValor( e.target.value ) }
+            onChange={alterarValor}
             type="text" />
+
+            <h4>Valor digitado: {valor}</h4>
+
         </div>
     )
 }
