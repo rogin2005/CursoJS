ed6c8ca5b38cc35f31bc5b2f2d193c64b561a158
Roger Ferreira Saraiva <rogerferreirasaraiva@gmail.com>
2026-05-23 19:55:48 -0300

    Colunas tailwind

---
+++ CursoJS/Fs_udemy/react-next-aula/next-tailwind/src/app/colunas/page.tsx
@@ -0,0 +1,16 @@
+
+export default function Aplicacao() {
+    return (
+        // <div className="p-2 columns-3 gap-8">
+        <div className="p-2 columns-1 md:columns-2 lg:columns-3"> {/* media queries md = medio 768px e lg = large 1024px */}
+
+            <div className="w-32 h-32 border bg-blue-500">Item1</div>
+            <div className="w-32 h-32 border bg-blue-500">Item2</div>
+            <div className="w-32 h-32 border bg-blue-500">Item3</div>
+            <div className="w-32 h-32 border bg-blue-500">Item4</div>
+            <div className="w-32 h-32 border bg-blue-500">Item5</div>
+            <div className="w-32 h-32 border bg-blue-500">Item6</div>
+
+        </div>
+    )
+}
