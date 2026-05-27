a7540ca9e07896a1b5388235c43e97af1e416575
Roger Ferreira Saraiva <rogerferreirasaraiva@gmail.com>
2026-05-27 12:52:44 -0300

    Posicionamentos static e relative

---
+++ CursoJS/Fs_udemy/react-next-aula/next-tailwind/src/app/posicionamento/page.tsx
@@ -0,0 +1,24 @@
+
+export default function Aplicacao() {
+    return (
+        // <div className="p-2 columns-3 gap-8">
+        <div className="border p-3"> 
+
+            
+
+            <p className="bg-orange-400 m-2">
+                ITEM DE DESTAQUE Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi iure dolor libero excepturi temporibus architecto, voluptatum voluptas eligendi dolorum velit! Dolore ipsam exercitationem libero numquam voluptatum, nesciunt amet odit quisquam.
+            </p>
+
+            {/* <p className="bg-purple-600 m-2 relative top-4 left-4 bottom-4 right-4"> */}
+            <p className="bg-purple-600 m-2 relative left-4">
+                Item 1 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi iure dolor libero excepturi temporibus architecto, voluptatum voluptas eligendi dolorum velit! Dolore ipsam exercitationem libero numquam voluptatum, nesciunt amet odit quisquam.
+            </p>
+
+            <p className="bg-gray-400 m-2">
+                Item 2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias ducimus sint iure sed cupiditate ipsum soluta. Dicta, exercitationem ex sequi, necessitatibus, quod incidunt sunt tempore quas suscipit natus labore? Cumque!
+            </p>
+
+        </div>
+    )
+}
