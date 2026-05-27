1929ae8b593dfbda8def4b923b2ee325f1dd5af2
Roger Ferreira Saraiva <rogerferreirasaraiva@gmail.com>
2026-05-27 16:36:03 -0300

    Posicionamentos - absolute, fixed e sticky

--- CursoJS/Fs_udemy/react-next-aula/next-tailwind/src/app/page.tsx
+++ CursoJS/Fs_udemy/react-next-aula/next-tailwind/src/app/page.tsx
@@ -1,6 +1,6 @@
 
 export default function Home() {
   return (
-    <h1>Página inicial!</h1>
+    <div className="minha-classe">Página principal</div>
   );
 }
--- CursoJS/Fs_udemy/react-next-aula/next-tailwind/src/app/posicionamento/page.tsx
+++ CursoJS/Fs_udemy/react-next-aula/next-tailwind/src/app/posicionamento/page.tsx
@@ -6,17 +6,19 @@ export default function Aplicacao() {
 
             
 
-            <p className="bg-orange-400 m-2">
+            {/* <p className="bg-orange-400 m-2 absolute top-2"> */}
+            {/* <p className="bg-orange-400 m-2 fixed bottom-2"> */}
+            <p className="bg-orange-400 m-2 sticky top-4 z-10">
                 ITEM DE DESTAQUE Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi iure dolor libero excepturi temporibus architecto, voluptatum voluptas eligendi dolorum velit! Dolore ipsam exercitationem libero numquam voluptatum, nesciunt amet odit quisquam.
             </p>
 
             {/* <p className="bg-purple-600 m-2 relative top-4 left-4 bottom-4 right-4"> */}
-            <p className="bg-purple-600 m-2 relative left-4">
-                Item 1 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi iure dolor libero excepturi temporibus architecto, voluptatum voluptas eligendi dolorum velit! Dolore ipsam exercitationem libero numquam voluptatum, nesciunt amet odit quisquam.
+            <p className="bg-purple-600 m-2 sticky top-4 z-20">
+                Item 1 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi iure dolor libero excepturi temporibus architecto, voluptatum voluptas eligendi dolorum velit! Dolore ipsam exercitationem libero numquam voluptatum, nesciunt amet odit quisquam. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui sapiente incidunt deserunt deleniti quam, quaerat pariatur. Rerum velit cum illum, unde nisi ab nobis dolorum! Animi vel soluta sapiente maiores!
             </p>
 
-            <p className="bg-gray-400 m-2">
-                Item 2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias ducimus sint iure sed cupiditate ipsum soluta. Dicta, exercitationem ex sequi, necessitatibus, quod incidunt sunt tempore quas suscipit natus labore? Cumque!
+            <p className="bg-gray-400 m-2 sticky top-4 z-30">
+                Item 2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias ducimus sint iure sed cupiditate ipsum soluta. Dicta, exercitationem ex sequi, necessitatibus, quod incidunt sunt tempore quas suscipit natus labore? Cumque! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo fugit quis saepe modi natus dolor incidunt laboriosam repellat ab eum, obcaecati doloremque adipisci cum assumenda nam! Similique hic error in? Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem modi alias vero iste qui architecto. Cumque, facere aperiam. Dolorum ullam veniam adipisci quis rerum voluptatem, ex nulla reiciendis voluptatum nesciunt.
             </p>
 
         </div>
