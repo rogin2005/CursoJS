d952d788a94c098af3b93c0ab48731958e3f2cf0
Roger Ferreira Saraiva <rogerferreirasaraiva@gmail.com>
2026-05-31 15:45:40 -0300

    Tailwind - Flex - direções e espaçamentos

---
+++ CursoJS/Fs_udemy/react-next-aula/next-tailwind/src/app/flexbox/page.tsx
@@ -0,0 +1,12 @@
+
+export default function Aplicacao() {
+    return (
+        // <div className="p-2 columns-3 gap-8">
+        // flex flex-row-reverse flex-col flex-wrap
+        <div className="border-4 p-3 flex flex-row gap-2"> 
+            <div className="caixa basis-64">Caixa 1</div>
+            <div className="caixa basis-96">Caixa 2</div>
+            <div className="caixa basis-64">Caixa 3</div>
+        </div>
+    )
+}
