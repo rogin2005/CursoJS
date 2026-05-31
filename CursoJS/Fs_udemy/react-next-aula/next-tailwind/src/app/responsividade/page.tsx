4467c01777e11bfe5c045d27a9c93c99dd444f17
Roger Ferreira Saraiva <rogerferreirasaraiva@gmail.com>
2026-05-31 17:06:34 -0300

    Prática com responsividade - Breakpoints

---
+++ CursoJS/Fs_udemy/react-next-aula/next-tailwind/src/app/responsividade/page.tsx
@@ -0,0 +1,16 @@
+export default function Responsividade() {
+    return (
+        <div>
+            <h1 
+            className="
+            bg-slate-300 
+            md:bg-orange-300
+            lg:bg-blue-800
+            xl:bg-yellow-300
+            2xl:bg-purple-900
+            ">
+                Responsividade
+            </h1>
+        </div>
+    )
+}
