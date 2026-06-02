d3482f87be0fde8a28b0ce3945c9c97eb9151173
Roger Ferreira Saraiva <rogerferreirasaraiva@gmail.com>
2026-06-02 12:10:39 -0300

    Tailwind - Flex - Redimensionamento

--- CursoJS/Fs_udemy/react-next-aula/next-tailwind/src/app/flexbox/page.tsx
+++ CursoJS/Fs_udemy/react-next-aula/next-tailwind/src/app/flexbox/page.tsx
@@ -4,10 +4,11 @@ export default function Aplicacao() {
         // <div className="p-2 columns-3 gap-8">
         // flex flex-row-reverse flex-col flex-wrap
         // order-2 grow justify-evenly items-start
-        <div className="border-4 p-3 h-screen flex flex-row gap-2 justify-evenly items-center"> 
-            <div className="caixa h-96">Caixa 1</div>
-            <div className="caixa">Caixa 2</div>
-            <div className="caixa">Caixa 3</div>
+        // <div className="border-4 p-3 h-screen flex flex-row gap-2 justify-evenly items-center">
+        <div className="border-4 p-3 flex gap-2">
+            <div className="caixa w-48 grow-0 shrink-0">Caixa 1</div>
+            <div className="caixa w-72 grow shrink">Caixa 2</div>
+            <div className="caixa w-96 grow-0 shrink-0">Caixa 3</div>
         </div>
     )
 }
