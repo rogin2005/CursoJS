997324b535dbfe8e6c5ecac5eb943a0070512d3a
Roger Ferreira Saraiva <rogerferreirasaraiva@gmail.com>
2026-06-04 17:00:48 -0300

    Tailwind - Espaçamentos - margin e padding

---
+++ CursoJS/Fs_udemy/react-next-aula/next-tailwind/src/app/espacamentos/page.tsx
@@ -0,0 +1,34 @@
+export function Aplicacao2() {
+    return (
+        <div>
+
+            {/* 
+            p-2, pl-4, px-4, py-4, p-[40px], md:p-4
+            */}
+            
+            <div className="bg-blue-300 p-2 md:p-4 lg:p-6">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem nihil inventore fuga corporis eius similique voluptatem, laboriosam fugiat quisquam nulla temporibus rem eum voluptas magnam ipsum optio veniam distinctio ex!
+            </div>
+
+        </div>
+    )
+}
+
+
+export default function Aplicacao() {
+    return (
+        <div>
+
+            {/* 
+            m-5, mt-5, mx-5, my-5, m-auto, m-[20px], 
+            */}
+            
+            <div className="bg-blue-300 m-[20px]">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem nihil inventore fuga corporis eius similique voluptatem, laboriosam fugiat quisquam nulla temporibus rem eum voluptas magnam ipsum optio veniam distinctio ex!
+            </div>
+
+            <h1 className="bg-red-500">Teste</h1>
+
+        </div>
+    )
+}
