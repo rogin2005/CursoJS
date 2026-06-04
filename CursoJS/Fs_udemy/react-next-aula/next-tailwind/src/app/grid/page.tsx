e993ae699932143f42f3fef4938cb622dfd3e4cf
Roger Ferreira Saraiva <rogerferreirasaraiva@gmail.com>
2026-06-04 19:42:01 -0300

    Tailwind - Grid - Responsividade

--- CursoJS/Fs_udemy/react-next-aula/next-tailwind/src/app/grid/page.tsx
+++ CursoJS/Fs_udemy/react-next-aula/next-tailwind/src/app/grid/page.tsx
@@ -1,8 +1,10 @@
 
 export default function Aplicacao() {
     return (
-        <div className="border-4 p-3 grid grid-cols-4 gap-2">
-            <div className="caixa col-start-2 col-end-4">Caixa 1</div>
+        // <div className="border-4 p-3 grid grid-cols-4 gap-2">
+        <div className="border-4 p-3 grid gap-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {/* <div className="caixa col-start-2 col-end-4">Caixa 1</div> */}
+            <div className="caixa col-span-1 md:col-span-2 lg:col-span-3 xl:col-span-4">Caixa 1</div>
             <div className="caixa">Caixa 2</div>
             <div className="caixa">Caixa 3</div>
             <div className="caixa">Caixa 4</div>
@@ -10,7 +12,7 @@ export default function Aplicacao() {
             <div className="caixa">Caixa 6</div>
             <div className="caixa">Caixa 7</div>
             <div className="caixa">Caixa 8</div>
-            <div className="caixa col-span-3">Caixa 9</div>
+            <div className="caixa">Caixa 9</div>
         </div>
     )
 }
