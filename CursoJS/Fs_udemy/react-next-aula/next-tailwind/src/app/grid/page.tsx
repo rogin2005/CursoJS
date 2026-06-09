509af8bf323f799bc0936d30df0a8a7972cfabc3
Roger Ferreira Saraiva <rogerferreirasaraiva@gmail.com>
2026-06-09 15:55:10 -0300

    Tailwind - Grid - Alinhamentos

--- CursoJS/Fs_udemy/react-next-aula/next-tailwind/src/app/grid/page.tsx
+++ CursoJS/Fs_udemy/react-next-aula/next-tailwind/src/app/grid/page.tsx
@@ -2,10 +2,15 @@
 export default function Aplicacao() {
     return (
         // <div className="border-4 p-3 grid grid-cols-4 gap-2">
-        <div className="border-4 p-3 grid gap-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        // <div className="border-4 p-3 grid gap-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        // justify-items-start, justify-items-stretch, justify-items-end, justify-items-center
+        // content-start, content-end, content-center, content-evenly, 
+        // items-start, items-end, items-center
+        <div className="border-4 p-3 h-screen grid gap-2 grid-cols-4 content-evenly items-baseline">
             {/* <div className="caixa col-start-2 col-end-4">Caixa 1</div> */}
-            <div className="caixa col-span-1 md:col-span-2 lg:col-span-3 xl:col-span-4">Caixa 1</div>
-            <div className="caixa">Caixa 2</div>
+            {/* <div className="caixa col-span-1 md:col-span-2 lg:col-span-3 xl:col-span-4">Caixa 1</div> */}
+            <div className="caixa">Caixa 1</div>
+            <div className="caixa h-52">Caixa 2</div>
             <div className="caixa">Caixa 3</div>
             <div className="caixa">Caixa 4</div>
             <div className="caixa">Caixa 5</div>
