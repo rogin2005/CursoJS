40829f791c66ba4996291f4636a02f81a0a9d83c
Roger Ferreira Saraiva <rogerferreirasaraiva@gmail.com>
2026-06-09 21:46:08 -0300

    Bordas pt1

---
+++ CursoJS/Fs_udemy/react-next-aula/next-tailwind/src/app/bordas/page.tsx
@@ -0,0 +1,12 @@
+
+export default function Aplicacao() {
+    return (
+        <div>
+            {/* border-2, border-orange-500, border-t-2, border-y-2, border-s-2 */}
+            {/* rounded-lg,  */}
+            <div className="m-8 border-2 rounded-lg border-orange-500">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem amet enim sequi at debitis est distinctio ex dolorum voluptatibus possimus temporibus, vero iure deserunt quod asperiores, voluptatem labore, ratione dolore!
+            </div>
+        </div>
+    )
+}
