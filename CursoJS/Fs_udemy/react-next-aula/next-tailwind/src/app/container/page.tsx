074892b5a0ecb72b4da8f1d58868babf163892bf
Roger Ferreira Saraiva <rogerferreirasaraiva@gmail.com>
2026-06-10 08:55:28 -0300

    Tailwind - Container

---
+++ CursoJS/Fs_udemy/react-next-aula/next-tailwind/src/app/container/page.tsx
@@ -0,0 +1,18 @@
+
+export default function Aplicacao() {
+    return (
+        <div className="container mx-auto bg-green-300 border-3">
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit voluptatibus impedit adipisci eius harum rerum possimus nostrum nesciunt dolor dignissimos, voluptates neque. Perferendis soluta, blanditiis molestiae ut consequatur nemo? Maxime?</p>
+            
+            <div className="border-4 p-3 grid gap-2 grid-cols-4">
+                <div className="caixa">Caixa 1</div>
+                <div className="caixa">Caixa 2</div>
+                <div className="caixa">Caixa 3</div>
+            </div>
+
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas ipsum consectetur distinctio delectus autem. Architecto atque eaque voluptas voluptatum blanditiis eligendi ea! Ratione amet nesciunt modi id esse eveniet expedita?</p>
+
+        </div>
+    )
+}
